a8673312497f6a6d6c4b847cbf8919f6670abde0
RishithaRo <rishithap129@gmail.com>
2024-11-29 15:37:10 +0530

    Create Hint2.java

---
+++ Hint2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Hint2
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        float c = sc.nextFloat();
+        System.out.print(a+"\n"+b);
+        System.out.print(c);
+    }
+}
