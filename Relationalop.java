f0a6f9b86551c07b05a88b718a636093ed442faf
RishithaRo <rishithap129@gmail.com>
2024-11-29 14:52:03 +0530

    Create Relationalop.java

---
+++ Relationalop.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Relationalop
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        boolean c = a<b;
+        boolean d = a>b;
+        boolean f = a<=b;
+        boolean g = a>=b;
+        boolean h = a==b;
+        boolean i = a!=b;
+        System.out.println(c);
+        System.out.println(d);
+        //System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+    }
+}
