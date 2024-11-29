f04e031f989fd6ab461b9f950566d72b001b05fc
RishithaRo <rishithap129@gmail.com>
2024-11-29 14:35:18 +0530

    Create Arithop.java

---
+++ Arithop.java
@@ -0,0 +1,22 @@
+// write a program that demonstrates arithmetic operators in java
+
+import java.util.*;
+public class Arithop
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        int f = a/b;
+        int g = a%b;
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
