21711d226aceb115131e8abe18095e5ffce03962
RishithaRo <rishithap129@gmail.com>
2024-12-04 15:33:15 +0530

    Create Class1.java

---
+++ Class1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Terv
+{
+    int a = 5;
+    int b = 10;
+}
+
+public class Class1
+{
+    public static void main(String args[])
+    {
+        Terv ob = new Terv();
+        System.out.println(ob.a);
+        System.out.println(ob.b);
+    }
+}
