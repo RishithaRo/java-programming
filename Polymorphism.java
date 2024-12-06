1f0066442fa66d16cffd1c17bc025b6546b699a0
RishithaRo <rishithap129@gmail.com>
2024-12-06 14:53:37 +0530

    Create Polymorphism.java

---
+++ Polymorphism.java
@@ -0,0 +1,23 @@
+// Polymorphism
+/*
+polymorphism means many ways to execute methods .
+Polymorphism is classified into 2 types.
+They are 
+    1. Compile-time Polymorphism
+    2. Run-time Polymorphism
+    
+==> Compile-time Polymorphism (method Overloading)
+            --> If a class has multiple methods by same name, It is called as compile time Polymorphism
+            --> It increase the readability of a program.
+        Compile time polymorphism can be achieved 
+            1) By changing the number of arguments
+            2) By changing Datatype.
+            
+==> Run-time Polymorphism (method Overriding)
+            --> If the child class has the same method declared in the parent class, It is called as method Overriding.
+        Rules for creating method Overriding
+            1) Method name must be same name in both parent and child class.
+            2) Methods must have same parameters
+            3) It should be achieved by inheritance method.( By extends keyword).
+            
+*/
