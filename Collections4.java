0b7000fb84e3b735cf63fef5ff3c846316851512
RishithaRo <rishithap129@gmail.com>
2024-12-14 16:13:01 +0530

    Create Collections4.java

---
+++ Collections4.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Collections4
+{
+    public static void main(String args[])
+    {
+        ArrayList<Integer> ob = new ArrayList<>();
+        ob.add(10);
+        ob.add(20);
+        ob.add(30);
+        System.out.println(ob);
+    }
+}
