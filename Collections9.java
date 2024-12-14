00feb5bee703e4f03667f29c47fd7dd76fcfaf54
RishithaRo <rishithap129@gmail.com>
2024-12-14 16:39:46 +0530

    Create Collections9.java

---
+++ Collections9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Collections9 
+{
+	public static void main(String args[])
+	{
+		Vector ob = new Vector();
+		ob.add(10);
+		ob.add(20);
+		ob.add("rohith");
+		ob.add("3445");
+		ob.add(true);
+		ob.add(5.45);
+		ob.add('r');
+		System.out.println(ob);
+	}
+}
