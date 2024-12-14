d5cecaf51d75ac7a98889c29eb085dfe5ac26e49
RishithaRo <rishithap129@gmail.com>
2024-12-14 16:32:41 +0530

    Create Collections7.java

---
+++ Collections7.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Collections7 {
+	public static void main(String args[])
+	{
+		ArrayList ob = new ArrayList();
+	    ob.add(10);
+	    ob.add(20);
+	    ob.add(30);
+	    ob.add(3.4);
+	    ob.add("rishi");
+	    ob.add(true);
+	    ob.add('r');
+	    System.out.println(ob);
+	}
+}
