4e64e010605c7b424c34e83898ecbfe0747871a7
RishithaRo <rishithap129@gmail.com>
2024-12-14 16:36:42 +0530

    Create Collections8.java

---
+++ Collections8.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Collections8
+{
+	public static void main(String args[])
+	{
+		LinkedList ob = new LinkedList();
+		ob.add(10);
+		ob.add(20);
+		ob.add("rishi");
+		ob.add(5.4);
+		ob.add(true);
+		ob.add('r');
+		System.out.println(ob);
+	}
+}
