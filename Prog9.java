8ae94e6a7b3f3d3c36aba621b8e9b9e8f3e89830
RishithaRo <rishithap129@gmail.com>
2025-02-16 21:18:06 +0530

    Create Prog9.java

---
+++ Prog9.java
@@ -0,0 +1,8 @@
+class Prog9
+{
+  public static void main(String[] args)
+  {
+    System.out.println("These are main Escape characters in java:");
+    System.out.println("New Line character:\\n\nTabSpacce Character:\\t\nInserts single quotes:\\'\nInserts Double quotes:\\\" ");
+  }
+}
