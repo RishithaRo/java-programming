2bfa1045666643b366ba424f4e63adb288fb7a25
RishithaRo <rishithap129@gmail.com>
2025-02-16 21:04:37 +0530

    Create Prog2.java

---
+++ Prog2.java
@@ -0,0 +1,10 @@
+class Prog2
+{
+    public static void main(String[] args)
+  {
+    System.out.print("Hello, World!");
+    System.out.println("RohithRishi");
+    System.out.println("Hii");
+    System.out.println("Hello");
+  }
+}
