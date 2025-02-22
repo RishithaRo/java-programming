bb97228d7df8bf9abe7db697af76fc24f4d68e3f
RishithaRo <rishithap129@gmail.com>
2025-02-22 22:26:55 +0530

    Create Prog11.java

---
+++ Prog11.java
@@ -0,0 +1,18 @@
+class Prog11
+{
+	public static void main(String[] args)
+	{
+		int m1,m2,m3,m4,m5;
+		m1=45;
+		m2=65;
+		m3=87;
+		m4=76;
+		m5=88;
+		int total;
+		total = m1+m2+m3+m4+m5;
+		int avg;
+		avg = total/5;
+		System.out.println("Total Marks:"+total);
+		System.out.println("Average:"+avg);
+	}
+}
