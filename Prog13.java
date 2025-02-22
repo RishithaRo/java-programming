40e91d6fa35477ffbd62608cca72add6df212302
RishithaRo <rishithap129@gmail.com>
2025-02-22 22:29:42 +0530

    Create Prog13.java

---
+++ Prog13.java
@@ -0,0 +1,11 @@
+class Prog13
+{
+	public static void main(String[] args)
+	{
+		int x=20;
+		float y=8.2f;
+		char r = 'k';
+		String s = "rishi";
+		System.out.println(x+" "+r+" "+y+" "+s);
+	}
+}
