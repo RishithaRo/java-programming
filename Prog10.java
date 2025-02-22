3d029cc07ebe442a88d8e07e46a0f57c4eab8d86
RishithaRo <rishithap129@gmail.com>
2025-02-22 22:26:03 +0530

    Create Prog10.java

---
+++ Prog10.java
@@ -0,0 +1,10 @@
+class Prog10
+{
+	public static void main(String[] args)
+	{
+		System.out.println("North Rajupalem");
+		System.out.println("Kodavalur mandal");
+		System.out.println("Nellore District");
+		System.out.println("Andhrapradesh");
+	}
+}
