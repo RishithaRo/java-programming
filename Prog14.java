1794acd034aa573951993e914931e965204eace3
RishithaRo <rishithap129@gmail.com>
2025-02-22 22:30:54 +0530

    Create Prog14.java

---
+++ Prog14.java
@@ -0,0 +1,11 @@
+class Prog14
+{
+	public static void main(String[] args)
+	{
+		byte d = 19;	//can store only 2 values , size is 1 byte 
+		short x = 20245; //can store upto 5 values, size is 2 bytes
+		int r = 1000234567; // can store upto 9 digits, size is 4 bytes
+		long k = 127352436544477534L;
+		System.out.println(d+"\n"+x+"\n"+r+"\n"+k);
+	}
+}
