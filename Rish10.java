b75251d27addadd2b9d04c0108e75a8701276e3b
RishithaRo <rishithap129@gmail.com>
2025-04-12 22:46:12 +0530

    Create Rish10.java

---
+++ Rish10.java
@@ -0,0 +1,24 @@
+package Consistent;
+import java.util.*;
+class Rish10 
+{
+	public static void main(String[] args) 
+	{
+		Scanner sc = new Scanner(System.in);
+		// check if a number is a perfect number
+		int n = sc.nextInt();
+		int num = n;
+		int sum = 0;
+		int i = 1;
+		while(i<=n/2)
+		{
+			if(num%i==0)
+			{
+				sum = sum+i;
+			}
+			i++;
+		}
+		System.out.println(sum);
+	}
+
+}
