d846f2e118c0bdffed8fb5a1ba9fbfd2d167239f
RishithaRo <rishithap129@gmail.com>
2025-04-12 21:30:05 +0530

    Create Rish2.java

---
+++ Rish2.java
@@ -0,0 +1,17 @@
+package Consistent;
+import java.util.*;
+class Rish2 
+{
+	public static void main(String[] args) {
+	// print even numbers between 1 to 20
+	int i=1;
+	while(i<=20)
+	{
+		if(i%2==0)
+		{
+			System.out.println(i);
+		}
+		i++;
+	}
+}
+}
