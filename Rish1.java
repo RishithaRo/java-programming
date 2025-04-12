1dd390e7f6a3c9f9168d760601eabe35429b5c1d
RishithaRo <rishithap129@gmail.com>
2025-04-12 21:26:03 +0530

    Create Rish1.java

---
+++ Rish1.java
@@ -0,0 +1,18 @@
+package Consistent;
+import java.util.*;
+class Rish1 
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		// print all numbers from 1 to n
+		int i=1;
+		while(i<=n)
+		{
+			System.out.println(i);
+			i++;
+			
+		}
+	}
+}
